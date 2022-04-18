b99b3a231a9e0de3071d5f083c192574f17b3b27
HiteshRepo <pattanayak.hitesh03@gmail.com>
2022-04-18 20:39:28 +0530

    Trie with map

--- DSAlgo/trie/main.go
+++ DSAlgo/trie/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"github.com/hiteshrepo/learninggist/dsalgo/trie/trie_data_structure"
 	"log"
-	"os"
-	"path"
-	"strings"
 )
 
 func main() {
-	// allPrefixes := getAllPrefixes()
+	log.Println("--------Trie---------")
+	TestTrie()
+	log.Println("--------Trie With Map---------")
+	TestTrieWithMap()
+}
+
+func TestTrieWithMap() {
+	allPrefixes := getTestPrefixes()
+
+	trie := trie_data_structure.GetNewMapTrie()
+
+	for _, word := range allPrefixes {
+		trie.Insert(word)
+	}
+
+	fmt.Println(trie.Search("apps"))
+	fmt.Println(trie.Search("bac"))
+	fmt.Println(trie.Search("azer"))
+	fmt.Println(trie.StartsWith("ap"))
+	fmt.Println(trie.StartsWith("ba"))
+	fmt.Println(trie.StartsWith("l"))
+}
+
+func TestTrie() {
 	allPrefixes := getTestPrefixes()
 
 	trie := trie_data_structure.GetNewTrie()
@@ -30,26 +49,4 @@ func main() {
 
 func getTestPrefixes() []string {
 	return []string{"apple", "apps", "apxl", "bac", "bat"}
-}
-
-func getAllPrefixes() []string {
-	allPrefixes := make([]string, 0)
-	wd, err := os.Getwd()
-	pathToFile := path.Join(wd, "DSAlgo/trie/dict/sample_prefixes.txt")
-	file, err := os.Open(pathToFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		allPrefixes = append(allPrefixes, strings.TrimSpace(scanner.Text()))
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	return allPrefixes
 }
---
+++ DSAlgo/trie/trie_data_structure/trie_with_map.go
@@ -0,0 +1,93 @@
+package trie_data_structure
+
+import "strings"
+
+type TrieMapNode struct {
+	alphabet map[int]*TrieMapNode
+	last     bool
+}
+
+func GetNewTrieMapNode() *TrieMapNode {
+	return &TrieMapNode{
+		alphabet: make(map[int]*TrieMapNode),
+		last:     false,
+	}
+}
+
+func (t *TrieMapNode) containsKey(char rune) bool {
+	ascii := int(char)
+	return t.alphabet[ascii] != nil
+}
+
+func (t *TrieMapNode) get(char rune) *TrieMapNode {
+	ascii := int(char)
+	return t.alphabet[ascii]
+}
+
+func (t *TrieMapNode) put(char rune, node *TrieMapNode) {
+	ascii := int(char)
+	t.alphabet[ascii] = node
+}
+
+func (t *TrieMapNode) setEnd() {
+	t.last = true
+}
+
+func (t *TrieMapNode) isEnd() bool {
+	return t.last
+}
+
+type MapTrie struct {
+	root *TrieMapNode
+}
+
+func GetNewMapTrie() *MapTrie {
+	return &MapTrie{root: GetNewTrieMapNode()}
+}
+
+func (t *MapTrie) Insert(word string) {
+	node := t.root
+	copyWord := strings.ToLower(word)
+	letters := []rune(copyWord)
+
+	for _,letter := range letters {
+		if !node.containsKey(letter) {
+			node.put(letter, GetNewTrieMapNode())
+		}
+
+		node = node.get(letter)
+	}
+
+	node.setEnd()
+}
+
+
+func (t *MapTrie) Search(word string) bool {
+	node := t.root
+	copyWord := strings.ToLower(word)
+	letters := []rune(copyWord)
+
+	for _,letter := range letters {
+		if !node.containsKey(letter) {
+			return false
+		}
+		node = node.get(letter)
+	}
+
+	return node.isEnd()
+}
+
+func (t *MapTrie) StartsWith(prefix string) bool {
+	node := t.root
+	copyWord := strings.ToLower(prefix)
+	letters := []rune(copyWord)
+
+	for _,letter := range letters {
+		if !node.containsKey(letter) {
+			return false
+		}
+		node = node.get(letter)
+	}
+
+	return true
+}
