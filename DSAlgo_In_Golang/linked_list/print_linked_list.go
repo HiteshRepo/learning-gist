e57c2789dd7d64386ec3b0aeceeae460c6a72a3f
HiteshRepo <pattanayak.hitesh03@gmail.com>
2022-05-01 18:54:46 +0530

    Print Linked List

---
+++ DSAlgo_In_Golang/linked_list/print_linked_list.go
@@ -0,0 +1,15 @@
+package linked_list
+
+import "github.com/hiteshrepo/LearningGist/DSAlgo_In_Golang/utils/linked_list"
+
+func PrintLinkedList(head *linked_list.Node) []int {
+	node := head
+	ans := make([]int, 0)
+
+	for node != nil {
+		ans = append(ans, node.GetData())
+		node = node.Next()
+	}
+
+	return ans
+}
---
+++ DSAlgo_In_Golang/utils/linked_list/linked_list.go
@@ -0,0 +1,39 @@
+package linked_list
+
+type Node struct {
+	data int
+	next *Node
+}
+
+func GetNewNode(data int) *Node {
+	return &Node{data: data, next: nil}
+}
+
+func (n *Node) GetData() int {
+	return n.data
+}
+
+func (n *Node) Next() *Node {
+	return n.next
+}
+
+type LinkedList struct {
+	head *Node
+}
+
+func GetNewLinkedList(data int) *LinkedList {
+	return &LinkedList{head: GetNewNode(data)}
+}
+
+func (l *LinkedList) GetHead() *Node {
+	return l.head
+}
+
+func (l *LinkedList) AddFromArray(arr []int) {
+	node := l.head
+	for _,n := range arr {
+		newNode := GetNewNode(n)
+		node.next = newNode
+		node = node.next
+	}
+}
