e488e738638d0a7da4e4a527608622e6bbeb9dac
HiteshRepo <pattanayak.hitesh03@gmail.com>
2022-07-03 10:56:17 +0530

    Solve DP:CountBinaryStrings

--- DSAlgo_In_Golang/dp/counting_binary_strings.go
+++ DSAlgo_In_Golang/dp/counting_binary_strings.go
@@ -1,1 +1,23 @@
 package dp
+
+func CountBinaryStrings(length int) int {
+
+	previousOnes := 1
+	previousZeroes := 1
+
+	currentOnes := 0
+	currentZeroes := 0
+
+	i := 0
+	for i < length {
+		currentOnes = previousOnes + previousZeroes
+		currentZeroes = previousOnes
+
+		previousOnes = currentOnes
+		previousZeroes = currentZeroes
+
+		i += 1
+	}
+
+	return currentZeroes + currentOnes
+}
--- DSAlgo_In_Golang/dp/counting_binary_strings_test.go
+++ DSAlgo_In_Golang/dp/counting_binary_strings_test.go
@@ -1,1 +1,14 @@
-package dp
+package dp_test
+
+import (
+	"github.com/hiteshrepo/LearningGist/DSAlgo_In_Golang/dp"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCountBinaryStrings(t *testing.T) {
+	length := 5
+	expected := 21
+	actual := dp.CountBinaryStrings(length)
+	assert.Equal(t, expected, actual)
+}
